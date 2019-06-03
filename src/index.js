328484da9d5695c16f6493048c05459f4bb02780
LeahB8 <leahboyd08@gmail.com>
2019-06-03 13:30:50 +0100

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const taskForm = document.querySelector('#create-task-form')
+const tasksUl = document.querySelector('#tasks')
+const taskInput = document.querySelector('#new-task-description')
+const dropdownListEl = document.querySelector('#dropdownList')
+const taskDueDateInput = document.querySelector('#due-date')
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+// });
+ 
+
+const printTask = () => {
+  const taskLi = document.createElement('li')
+  
+  taskLi.innerHTML = `
+    ${taskInput.value}<br>
+    Due by: ${taskDueDateInput.value}
+    <button id="deleteBtn">X</button>
+  `
+  tasksUl.append(taskLi)
+  
+  const dropdownList = () => {
+    if (dropdownListEl.value === '#FF0000') {
+      taskLi.style.color = '#FF0000'
+    } if (dropdownListEl.value === '#FFFF00') {
+      taskLi.style.color = '#FFFF00'
+    } if (dropdownListEl.value === '#00FF00') {
+      taskLi.style.color = '#00FF00'
+    }
+  }
+  dropdownList()
+
+  taskLi.querySelector('#deleteBtn').addEventListener('click', () => {
+    taskLi.remove()
+  })
+
+}
+
+
+
+taskForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  printTask()
+  taskForm.reset()
+})
+
+
